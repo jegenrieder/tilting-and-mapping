446af9ffaa360085b48c09a41c0dae40dc7e9df6
Jim Egenrieder <jimegenrieder@gmail.com>
2022-08-09 20:48:16 -0400

    I took the titling code (with mapping) and added pitch and volume

--- main.ts
+++ main.ts
@@ -8,22 +8,30 @@ let y = 0
 let x = 0
 x = 2
 y = 2
+let HZ = 262
+let amplify = 127
+music.setVolume(amplify)
 led.plot(x, y)
 let pausetime = 500
 let sensitivity = 200
 basic.forever(function () {
+    music.ringTone(HZ)
     if (input.acceleration(Dimension.X) > sensitivity) {
         if (x < 4) {
             led.toggle(x, y)
             x = x + 1
+            HZ += 40
             led.toggle(x, y)
+            music.ringTone(HZ)
             basic.pause(pausetime)
         }
     } else if (input.acceleration(Dimension.X) < 0 - sensitivity) {
         if (x > 0) {
             led.toggle(x, y)
             x = x - 1
+            HZ += -40
             led.toggle(x, y)
+            music.ringTone(HZ)
             basic.pause(pausetime)
         }
     }
@@ -31,13 +39,18 @@ basic.forever(function () {
         if (y < 4) {
             led.toggle(x, y)
             y = y + 1
+            amplify += 40
+            music.setVolume(amplify)
             led.toggle(x, y)
+            music.ringTone(HZ)
             basic.pause(pausetime)
         }
     } else if (input.acceleration(Dimension.Y) < 0 - sensitivity) {
         if (y > 0) {
             led.toggle(x, y)
             y = y - 1
+            amplify += 40
+            music.setVolume(amplify)
             led.toggle(x, y)
             basic.pause(pausetime)
         }
@@ -46,5 +59,8 @@ basic.forever(function () {
         while (true) {
             basic.pause(1000)
         }
+    }
+    if (input.buttonIsPressed(Button.AB)) {
+        music.stopAllSounds()
     }
 })
